21bf8b982ac5596e40ca8ffbb51aadb9ed791d94
mushahid <mushahidhammadabbasi@gmail.com>
2024-12-19 13:17:58 +0500

    06-leapYears eercise completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isCenturyDivisibleByFourHundred = year % 400 === 0; 
 
+if (isYearDivisibleByFour && 
+(!isCentury || isCenturyDivisibleByFourHundred)) {
+    return true;
+}
+else {
+    return false;
+}
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
