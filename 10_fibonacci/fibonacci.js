89792a668f960245616e70cb66d5e0cdae91218a
mushahid <mushahidhammadabbasi@gmail.com>
2025-01-28 13:21:28 +0500

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
-};
+    n = Number(n);
+
+    if (n < 0) return "OOPS"; // Handling invalid cases
+    
+    if (n === 0) return 0;
+    if (n === 1 || n === 2) return 1; // First two numbers are 1
+
+    let a = 1, b = 1;
+    for (let i = 3; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
