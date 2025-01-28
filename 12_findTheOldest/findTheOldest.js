c4b769fa14d82c8fe9dd37d47244142204318b29
mushahid <mushahidhammadabbasi@gmail.com>
2025-01-28 13:36:05 +0500

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        // Get birth and death year, assuming living people die in the current year
+        const currentYear = new Date().getFullYear();
+        const personDeathYear = person.yearOfDeath || currentYear;
+        const personAge = personDeathYear - person.yearOfBirth;
 
+        const oldestDeathYear = oldest.yearOfDeath || currentYear;
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+
+        // Return the older person
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
